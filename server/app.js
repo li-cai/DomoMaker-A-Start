b86dda837d4d7551954464c60346b74af8a71f10
Cailin Li <cxl2467@rit.edu>
2017-04-03 14:27:30 -0400

    Implement login, sign up functionality

---
+++ server/app.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+
+// library to handle POST requests any information sent in an HTTP body
+const bodyParser = require('body-parser');
+// Mongoose is one of the most popular MongoDB libraries for node
+const mongoose = require('mongoose');
+// express handlebars is an express plugin for handlebars templating
+const expressHandlebars = require('express-handlebars');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+
+mongoose.connect(dbURL, (err) => {
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
+});
+
+const router = require('./router.js');
+
+const app = express();
+
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+
+router(app);
+
+// Tell the app to listen on the specified port
+app.listen(port, (err) => {
+    // if the app fails, throw the err
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
